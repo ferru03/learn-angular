50d862243d176133eedf9c803f79579b84067ab3
ferru03 <76011910+ferru03@users.noreply.github.com>
2022-06-06 22:33:01 +0200

    Delete learn-angular directory

--- learn-angular/src/app/Employee.ts
+++
@@ -1,3 +0,0 @@
-export class Employee{
-    constructor (public id?: number, public birthDate?: string, public firstName?: string, public lastName?: string, public gender?: string, public hireDate?: string){}
-}
